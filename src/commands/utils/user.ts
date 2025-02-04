14d2dac19f976ba2216e2d4cb84e3d06ae41a220
MiniGT <bendanillo539@gmail.com>
2025-02-04 15:09:14 +0800

    feat: add user slash command

---
+++ src/commands/utils/user.ts
@@ -0,0 +1,19 @@
+import { ChatInputCommandInteraction, GuildMember, SlashCommandBuilder } from 'discord.js';
+
+export default {
+	data: new SlashCommandBuilder()
+		.setName('user')
+		.setDescription('Provides information about the user.'),
+	async execute(interaction: ChatInputCommandInteraction) {
+    console.log(interaction)
+
+		const member = interaction.member as GuildMember | null;
+
+		const joinedDate = member?.joinedAt 
+			? member.joinedAt.toDateString() 
+			: 'N/A';
+		// interaction.user is the object representing the User who ran the command
+		// interaction.member is the GuildMember object, which represents the user in the specific guild
+		await interaction.reply(`This command was run by ${interaction.user.username}, who joined on ${joinedDate}.`);
+	},
+};
