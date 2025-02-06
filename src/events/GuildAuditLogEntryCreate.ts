813ac0572fea22d15dd83aed5a1cbe9a1a786c5f
MiniGT <bendanillo539@gmail.com>
2025-02-06 13:45:32 +0800

    feat: logs different message base on guild action chore(client): removed unused codes

--- src/events/GuildAuditLogEntryCreate.ts
+++ src/events/GuildAuditLogEntryCreate.ts
@@ -1,17 +1,9 @@
-import { AuditLogEvent, Client, Events, GatewayIntentBits } from "discord.js";
+import { AuditLogEvent, Events } from "discord.js";
 import 'dotenv/config';
+import getHour from "../globalUtils/getHour.js";
 
 const auditLogChannelId = process.env.AUDIT_LOG_CHANNEL_ID
 if(!auditLogChannelId) throw new Error('No audit log channel id provided')
-
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildModeration
-  ],
-});
 
 // listens to member bans
 export default {
@@ -29,14 +21,29 @@ export default {
       }); 
       const logsEntry = fetchedLogs.entries.first();
       
-      // Define your variables.
-	    const { action, executor, target } = logsEntry;
-      const executer = executor
-      const targetUser = target
+      // Define the variables.
+	    const { action, executor, target, reason } = logsEntry;
+      const executerName = executor
+      const auditAction = AuditLogEvent[action]
 
       // send the message in the audit log channel
       const auditLogChannel = await guild.channels.cache.get(auditLogChannelId)
-      auditLogChannel.send(`${executer.globalName}(${executer.username}) has ${AuditLogEvent[action]} ${targetUser.globalName}(${targetUser.username}).`)
+      switch(action) {
+        case 40: // 'Invite Create'
+          auditLogChannel.send(`${executerName.globalName}(${executerName.username}) has created an invite. Invitation code: ${target.code}, Expiration: ${target.maxAge ? `${getHour(target.maxAge)}hr/s` : 'Never'}, Max Uses: ${target.maxUses ? target.maxUses : 'No Limit'}.`)
+          break
+        case 20: // Member Kick
+          auditLogChannel.send(`${executerName.globalName}(${executerName.username}) has ${auditAction} ${target.globalName}(${target.username}). Reason: ${reason ? reason : 'None'}`)
+          break
+        case 22: // Member Ban
+          auditLogChannel.send(`${executerName.globalName}(${executerName.username}) has Banned ${target.globalName}(${target.username}). Reason: ${reason ? reason : 'None'}`)
+          break
+        case 23: // Remove Member Ban 
+          auditLogChannel.send(`${executerName.globalName}(${executerName.username}) has Removed the Ban of ${target.globalName}(${target.username}). Reason: ${reason ? reason : 'None'}`)
+          break
+        default:
+          auditLogChannel.send(`${executerName.globalName}(${executerName.username}) has ${auditAction} ${target.globalName ? target.username : ''}.`)
+      }
       
     } catch (error) {
       console.log(error)
