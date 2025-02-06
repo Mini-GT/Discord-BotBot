d1824cf462fab865f380c4ffdc32770bdd230f56
MiniGT <bendanillo539@gmail.com>
2025-02-06 13:49:01 +0800

    fix: made message content to handle all upper and lower case

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -3,7 +3,7 @@ import { Events, Message } from "discord.js";
 export default {
   name: Events.MessageCreate,
   async execute(message: Message) {
-    if (message.content === '!ping') {
+    if (message.content.toLowerCase() === '!ping') {
         message.reply('Pong!');
     }
   }
