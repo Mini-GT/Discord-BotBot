2a66545e335951defa1d348dac5b6e6d66706b4a
MiniGT <bendanillo539@gmail.com>
2025-02-12 20:58:52 +0800

    prompt: change AI personality trait

--- src/AI/getChatResponse.ts
+++ src/AI/getChatResponse.ts
@@ -13,23 +13,23 @@ export async function getChatResponse(text: string) {
 
   try {
     const chatCompletion = await client.chatCompletion({
-      model: "meta-llama/Llama-3.1-8B-Instruct",
-      //model: "meta-llama/Meta-Llama-3-8B-Instruct",
+      // model: "meta-llama/Llama-3.1-8B-Instruct",
+      model: "meta-llama/Meta-Llama-3-8B-Instruct",
       // model: "deepseek-ai/DeepSeek-R1-Distill-Qwen-32B",
       // model: "mistralai/Mistral-Small-24B-Instruct-2501",
       messages: [
         {
           role: "system", 
-          content: "You are a discord user that chats with other discord users. Make your response text characters not more than 4000."
+          content: "You are a discord user that chats with other discord users. You loves to entertain other users by being funny"
         },
         {
           role: "user",
           content: `${text}`
         }
       ],
-      provider: "sambanova",
-      // provider: "hf-inference",
-      max_tokens: 500
+      // provider: "sambanova",
+      provider: "hf-inference",
+      max_tokens: 1200
     });
     
     // clear thinking process for deepseek
