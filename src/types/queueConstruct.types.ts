f15df247767f736a2f16d7916b52a888ad1f6594
MiniGT <bendanillo539@gmail.com>
2025-02-15 23:09:47 +0800

    types: add QueueConstruct type

---
+++ src/types/queueConstruct.types.ts
@@ -0,0 +1,15 @@
+type Song = {
+  title: string
+  url: string
+  duration: string
+  requestedBy: string
+}
+
+export type QueueConstructType = {
+  textChannel: any;
+  voiceChannel: any;
+  connection: any;
+  songs: Song[];
+  playing: boolean;
+}
+
