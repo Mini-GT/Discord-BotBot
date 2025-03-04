bc156064071eb23ee311f6d479e8dcc5b8376f29
MiniGT <bendanillo539@gmail.com>
2025-03-04 14:18:10 +0800

    chore: delete unused folder

--- src/globalUtils/play/playSong.ts
+++
@@ -1,6 +0,0 @@
-import { createAudioPlayer, createAudioResource } from '@discordjs/voice';
-import ytdl from '@distube/ytdl-core';
-import { EmbedBuilder } from 'discord.js';
-import { queue } from '../../events/messageCreate.js';
-
-// export
